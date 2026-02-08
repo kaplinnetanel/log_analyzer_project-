869be411d13acc69be8bcad3cb60352720a96461
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-02-08 10:39:58 +0200

    שינוי פונקציה 1

---
+++ reader.py
@@ -0,0 +1,11 @@
+import csv
+
+def load_csv_to_list(file_path):
+    """מקבלת נתיב לקובץ
+     csv
+    ומחזירה רשימה של רשימות - כל שורה
+    כרשימה של שדות.
+        """
+    with open(file_path, "r", newline="") as f:
+        reader = csv.reader(f)
+        return list(reader)
