d71495f6cb1949d87ed5008186c1ce031634c7da
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-02-08 12:26:41 +0200

    function5

--- checks.py
+++ checks.py
@@ -4,9 +4,13 @@ from reader import *
 #     return [a for a in data if not a[1].startswith(("10","192.168")) ]
 # print(extracting_ip_addresses(log_list))
 
-def filter_by_sensitive_port(data):
-    return [a for a in data if   a[3] != 23 or 3389 or 22]
+# def filter_by_sensitive_port(data):
+#     return [a for a in data if   a[3] != 23 or 3389 or 22]
+#
+# def filter_by_size(data):
+#     return [a for a in data if int(a[-1]) > 5000]
+# print(filter_by_size(log_list))
 
-def filter_by_size(data):
-    return [a for a in data if int(a[-1]) > 5000]
-print(filter_by_size(log_list))
+def tag_traffic_by_port(data):
+    return [ "NORMAL" if int(a[-1]) > 500 else "LARGE" for a in data ]
+print(tag_traffic_by_port(log_list))
