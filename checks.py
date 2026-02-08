313a652d9330a6cbf1822865866f01e1726a8cad
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-02-08 11:49:22 +0200

    function4

--- checks.py
+++ checks.py
@@ -1,8 +1,12 @@
 from reader import *
 
-def extracting_ip_addresses(data):
-    return [a for a in data if not a[1].startswith(("10","192.168")) ]
-print(extracting_ip_addresses(log_list))
+# def extracting_ip_addresses(data):
+#     return [a for a in data if not a[1].startswith(("10","192.168")) ]
+# print(extracting_ip_addresses(log_list))
 
 def filter_by_sensitive_port(data):
     return [a for a in data if   a[3] != 23 or 3389 or 22]
+
+def filter_by_size(data):
+    return [a for a in data if int(a[-1]) > 5000]
+print(filter_by_size(log_list))
